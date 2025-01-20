1c286755a8f7495c577a49cd47dae7aa4afd6cf1
xmrtdao <xmrtsolutions@gmail.com>
2025-01-20 11:15:50 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mobilemonero
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/xmrtdao/mobilemonero)
---
+++ src/translations.ts
@@ -0,0 +1,24 @@
+export const translations = {
+  en: {
+    step1Title: 'Install Termux',
+    step1Description: 'First, download and install Termux from your device\'s app store.',
+    step2Title: 'Install Python',
+    step2Description: 'Open Termux and run the following command to install Python:',
+    step3Title: 'Verify Installation',
+    step3Description: 'Check your Python installation by running:',
+    next: 'Next',
+    previous: 'Previous',
+    support: 'Need help? Contact support@xmrtsolutions.com'
+  },
+  es: {
+    step1Title: 'Instalar Termux',
+    step1Description: 'Primero, descarga e instala Termux desde la tienda de aplicaciones de tu dispositivo.',
+    step2Title: 'Instalar Python',
+    step2Description: 'Abre Termux y ejecuta el siguiente comando para instalar Python:',
+    step3Title: 'Verificar Instalación',
+    step3Description: 'Verifica tu instalación de Python ejecutando:',
+    next: 'Siguiente',
+    previous: 'Anterior',
+    support: '¿Necesitas ayuda? Contacta a support@xmrtsolutions.com'
+  }
+};
